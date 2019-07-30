4ffece90f093356e682e7d91fa88c1a07bf17172
ses10 <sesma@live.com>
2019-07-29 20:52:31 -0400

    initial repo

---
+++ src/main/java/com/ses/articleSite/ArticleSiteApplication.java
@@ -0,0 +1,13 @@
+package com.ses.articleSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleSiteApplication.class, args);
+	}
+
+}
