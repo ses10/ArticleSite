a461beade6eeebfa8585518bc13ed966e4864bc7
ses10 <sesma@live.com>
2019-07-29 23:27:04 -0400

    pagination for article resource

--- src/main/java/com/ses/articleSite/controller/ArticleController.java
+++ src/main/java/com/ses/articleSite/controller/ArticleController.java
@@ -1,21 +1,34 @@
 package com.ses.articleSite.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ses.articleSite.repository.ArticleRepository;
+import com.ses.articleSite.model.Article;
+import com.ses.articleSite.service.ArticleService;
 
 @RestController
 public class ArticleController {
 	
-	@Autowired
-	ArticleRepository repo;
+	private ArticleService articleService;
 	
-	@GetMapping
+	@Autowired
+	public ArticleController(ArticleService articleService) {
+		this.articleService = articleService;
+	}
+	
+	@GetMapping(path="ping")
 	public String ping() {
-		System.out.println(repo.findAll().size());
 		return "Service is running...";
+	}
+	
+	@GetMapping(path="/articles", params = {"page", "size"}, produces = "application/json")
+	public List<Article> findArticles(@RequestParam("page") int page, @RequestParam("size") int size) {
+		return articleService.findAll(PageRequest.of(page, size));
 	}
 	
 }
